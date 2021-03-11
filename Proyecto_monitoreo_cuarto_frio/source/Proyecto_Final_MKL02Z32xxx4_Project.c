bdb49085c26b408a69d2bd800b12ee986df1abba
Willis jose jimenez naranjo <willisjimenezjn@unimagdalena.edu.co>
2021-03-10 20:07:00 -0500

    Modifica main para prueba de libreiras UART0 y GPIO
    Agrega nuevo comentario

--- Proyecto_monitoreo_cuarto_frio/source/Proyecto_Final_MKL02Z32xxx4_Project.c
+++ Proyecto_monitoreo_cuarto_frio/source/Proyecto_Final_MKL02Z32xxx4_Project.c
@@ -1,37 +1,14 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
+/*! @file : sdk_hal_gpio.c
+ * @author  Alvaro Augusto Narvaez Yepez -- Willis Jose Jimenez Naranjo
+ * @version 1.0.0
+ * @date    7 mar. 2021
+ * @brief   Funcion principal main
+ * @details
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
-/**
- * @file    Proyecto_Final_MKL02Z32xxx4_Project.c
- * @brief   Application entry point.
- */
+*/
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,6 +16,11 @@
 #include "clock_config.h"
 #include "MKL02Z4.h"
 #include "fsl_debug_console.h"
+
+#include "sdk_hal_uart0.h"
+#include "sdk_hal_gpio.h"
+
+
 /* TODO: insert other include files here. */
 
 /* TODO: insert other definitions and declarations here. */
@@ -47,6 +29,8 @@
  * @brief   Application entry point.
  */
 int main(void) {
+	status_t status;
+	uint8_t nuevo_byte_uart;
 
   	/* Init board hardware. */
     BOARD_InitBootPins();
@@ -56,17 +40,41 @@ int main(void) {
     /* Init FSL debug console. */
     BOARD_InitDebugConsole();
 #endif
+    (void)uart0Inicializar(115200); //115200bps
 
-    PRINTF("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
+    PRINTF("Usar teclado para controlar LEDs\r\n");
+    PRINTF("r-R led ROJO\r\n");
+    PRINTF("v-V led VERDE\r\n");
+    PRINTF("a-A led AZUL\r\n");
     while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
+      	if(uart0CuantosDatosHayEnBuffer()>0){
+      		status=uart0LeerByteDesdeBuffer(&nuevo_byte_uart);
+      		if(status==kStatus_Success){
+      			printf("dato:%c\r\n",nuevo_byte_uart);
+      			switch (nuevo_byte_uart) {
+  				case 'a':
+  				case 'A':
+  					gpioPutToggle(KPTB10);
+  					break;
+
+  				case 'v':
+  					gpioPutHigh(KPTB7);
+  					break;
+  				case 'V':
+  					gpioPutLow(KPTB7);
+  					break;
+
+  				case 'r':
+  					gpioPutValue(KPTB6,1);
+  					break;
+  				case 'R':
+  					gpioPutValue(KPTB6,0);
+  					break;
+  				}
+      		}else{
+      			printf("error\r\n");
+      		}
+      	}
     }
     return 0 ;
 }
